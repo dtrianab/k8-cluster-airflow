9fe22581904fff37aea388af436442dba9a5f03d
Diego Triana <da.trianab@gmail.com>
2022-05-17 00:28:25 +0200

    changes kind load line to use values instead of variables

--- redeploy-image.sh
+++ redeploy-image.sh
@@ -1,3 +1,3 @@
 docker build --pull --tag airflow-local-build:0.0.1 .
-kind load docker-image $IMAGE:$TAG
+kind load docker-image airflow-local-build:0.0.1
 helm upgrade airflow apache-airflow/airflow --namespace $NAMESPACE --set images.airflow.repository=airflow-local-build--set images.airflow.tag=0.0.1 --timeout 10m30s

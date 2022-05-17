4609065404ef0a3c132ab9f001df0f5427b0da2f
Diego Triana <da.trianab@gmail.com>
2022-05-17 15:39:53 +0200

    upd sh

--- first_deploy.sh
+++ first_deploy.sh
@@ -1,8 +1,8 @@
 export NAMESPACE=airflow-cluster
 export RELEASE_NAME=airflow
 kind create cluster --image kindest/node:v1.21.1
-helm repo add apache-airflow https://airflow.apache.org
+helm repo add apache-airflow https://airflow.apache.org --debug
 helm repo update
 kubectl create namespace $NAMESPACE
-helm install $RELEASE_NAME apache-airflow/airflow --namespace $NAMESPACE
+helm install $RELEASE_NAME apache-airflow/airflow --namespace $NAMESPACE --debug
 kubectl port-forward svc/$RELEASE_NAME-webserver 8080:8080 --namespace $NAMESPACE

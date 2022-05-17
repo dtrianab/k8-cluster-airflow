638eb665e7776db7c133bc58270262de8cceea56
Diego Triana <da.trianab@gmail.com>
2022-05-17 23:26:43 +0200

    Add rollout on deployment to refresh pods after image load

--- redeploy-image.sh
+++ redeploy-image.sh
@@ -5,4 +5,5 @@ set -x
 docker build --pull --tag airflow-local-build:0.0.1 .
 kind load docker-image airflow-local-build:0.0.1
 helm upgrade $RELEASE_NAME apache-airflow/airflow --namespace $NAMESPACE --set images.airflow.repository=airflow-local-build --set images.airflow.tag=0.0.1 --timeout 10m30s
+kubectl rollout restart deployment -n $NAMESPACE
 set +x
